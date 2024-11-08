9381bb8804687840875606f33b790bea8156b1ee
Lev Rusanov <30170278+JDM170@users.noreply.github.com>
2024-11-08 23:01:42 +0700

    Initial commit
    Signed-off-by: Lev Rusanov <30170278+JDM170@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace file_hash_checker
+{
+    internal class Program
+    {
+        private static string CalculateMD5(string data)
+        {
+            using (var md5 = MD5.Create())
+            {
+                //using (var progress = new ProgressBar())
+                //{
+                //    progress.Report((double)(stream.Position / stream.Length));
+                //}
+                Console.Write("Расчет MD5... ");
+                using (var stream = File.OpenRead(data))
+                    md5.ComputeHash(stream);
+                Console.WriteLine("Готово.");
+
+                StringBuilder sb = new StringBuilder();
+                byte[] hash = md5.Hash;
+                for (int i = 0; i < hash.Length; i++)
+                    sb.Append(hash[i].ToString("x2"));
+
+                return sb.ToString();
+            }
+        }
+
+        private static string InputData(string message)
+        {
+            Console.WriteLine(message);
+            Console.Write("> ");
+            string data = Console.ReadLine().Trim();
+            if (data.Length == 0 || data == string.Empty)
+                InputData(message);
+            return data;
+        }
+
+        static void Main()
+        {
+            string filename = InputData("\nВведите путь до файла:");
+            string remote = InputData("\nВведите путь до файла для сравнения (или путь до файла с хэшем или сам хэш):");
+            Console.WriteLine();
+            
+            string hash = CalculateMD5(filename);
+            bool result = false;
+            if (Regex.Match(remote, @"[a-zA-Z0-9]{32,}").Success)
+                result = hash == remote.ToLower();
+            else if (File.Exists(remote))
+                result = hash == CalculateMD5(remote);
+            else if (remote.EndsWith(".md5"))
+            {
+                if (!File.Exists(remote))
+                {
+                    Console.WriteLine("Файл не найден!");
+                    return;
+                }
+                var fileData = File.ReadAllText(remote);
+                Match match = Regex.Match(fileData, @"[a-zA-Z0-9]{32,}");
+                if (match.Success)
+                    result = hash == match.Value;
+            }
+
+            Console.WriteLine("\nСовпадает: " + (result ? "Да" : "Нет"));
+
+            Console.WriteLine("\nНажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Утилита для сверки контрольных сумм файлов")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("file_hash_checker")]
+[assembly: AssemblyCopyright("Lev Rusanov © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4fdab78e-f93e-4ddc-a787-b8ce6f14542f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
